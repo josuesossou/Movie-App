9ee81324ffb3a68041bd7fe1695d70a1c2aa8e57
Josue Sossou <josue@Macbooks-MacBook-Pro-5.local>
2018-05-03 01:18:06 -0400

    Updated .gitignore and added test directory

--- serverDbAuth/server/server.js
+++ serverDbAuth/server/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
+
+mongoose.connect('');
 
 app.get('/', (req, res) => {
     res.send('hello World');
